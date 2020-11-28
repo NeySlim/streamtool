acfdaccfccade3cbcb8ced6a2a51ec782f746dc5
NeySlim <lio@pew.pet>
2020-11-29 00:22:45 +0100

    Update functions.php

--- app/www/functions.php
+++ app/www/functions.php
@@ -98,7 +98,7 @@ function stop_stream($id)
 function secondsToTime($seconds) {
     $dtF = new \DateTime('@0');
     $dtT = new \DateTime("@$seconds");
-    return $dtF->diff($dtT)->format('%a days, %h hours, %i minutes and %s seconds');
+    return $dtF->diff($dtT)->format('%ad %hh %im %ss');
 }
 
 function getTranscode($id, $streamnumber = null)
