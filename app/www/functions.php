74d29eb3342fef883a31e37f9034e5381b231005
NeySlim <lio@pew.pet>
2021-02-09 15:46:36 +0100

    Update functions.php
    tuned probesize to speed up

--- app/www/functions.php
+++ app/www/functions.php
@@ -121,8 +121,8 @@ function getTranscode($id, $streamnumber = null)
     if ($trans) {
         $ffmpeg .= ' -y -thread_queue_size 512 -loglevel error -fflags nobuffer -flags low_delay -fflags +genpts -strict experimental -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 -err_detect ignore_err';
         $ffmpeg .= ' -progress /opt/streamtool/app/www/' . $setting->hlsfolder . '/' . $stream->id . '_.stats';
-        $ffmpeg .= ' -probesize ' . ($trans->probesize ? $trans->probesize : '15000000');
-        $ffmpeg .= ' -analyzeduration ' . ($trans->analyzeduration ? $trans->analyzeduration : '12000000');
+        $ffmpeg .= ' -probesize ' . ($trans->probesize ? $trans->probesize : '1000000');
+        $ffmpeg .= ' -analyzeduration ' . ($trans->analyzeduration ? $trans->analyzeduration : '1000000');
         $ffmpeg .= ' -user_agent "' . ($setting->user_agent ? $setting->user_agent : 'Streamtool') . '"';
         $nvencpos = 0;
         if (strpos($trans->video_codec, 'nvenc')) {
@@ -176,8 +176,8 @@ function getTranscodedata($id)
     $setting = Setting::first();
     $ffmpeg = $setting->ffmpeg_path;
     $ffmpeg .= ' -y -thread_queue_size 512 -loglevel error -fflags nobuffer -flags low_delay -fflags +genpts -strict experimental -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 2 -err_detect ignore_err';
-    $ffmpeg .= ' -probesize ' . ($trans->probesize ? $trans->probesize : '15000000');
-    $ffmpeg .= ' -analyzeduration ' . ($trans->analyzeduration ? $trans->analyzeduration : '12000000');
+    $ffmpeg .= ' -probesize ' . ($trans->probesize ? $trans->probesize : '1000000');
+    $ffmpeg .= ' -analyzeduration ' . ($trans->analyzeduration ? $trans->analyzeduration : '1000000');
     $ffmpeg .= ' -user_agent "' . ($setting->user_agent ? $setting->user_agent : 'Streamtool') . '"';
     $nvencpos = 0;
     if (strpos($trans->video_codec, 'nvenc')) {
@@ -227,7 +227,7 @@ function start_stream($id)
         $stream->status = 1;
     } else {
         $stream->checker = 0;
-        $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 9000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
+        $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 1000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
         $streaminfo = json_decode($checkstreamurl, true);
         if ($streaminfo) {
 //            $pid = shell_exec(getTranscode($stream->id));
@@ -262,7 +262,7 @@ function start_stream($id)
             if ($stream->streamurl2) {
                 $stream->checker = 2;
 
-                $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 9000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
+                $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 1000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
                 $streaminfo = json_decode($checkstreamurl, true);
 
                 if ($streaminfo) {
@@ -294,7 +294,7 @@ function start_stream($id)
                     }
                     if ($stream->streamurl3) {
                         $stream->checker = 3;
-                        $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 9000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
+                        $checkstreamurl = shell_exec('' . $setting->ffprobe_path . ' -analyzeduration 1000000 -probesize 1000000 -i "' . $stream->streamurl . '" -v  quiet -print_format json -show_streams 2>&1');
                         $streaminfo = json_decode($checkstreamurl, true);
                         if ($streaminfo) {
                 //            $pid = shell_exec(getTranscode($stream->id, 3));
